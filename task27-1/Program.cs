1b368289693bfab596e330e4bd91ca9a783bc6b5
tatangracheva <grachtany@yandex.ru>
2022-12-06 12:07:13 +0300

    выполнено упражнение 27

---
+++ task27-1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число - ");
+int mas = int.Parse(Console.ReadLine());
+int result = 0;
+while (mas > 0)
+{
+    int digit;
+    mas = Math.DivRem(mas, 10, out digit);
+    int m = digit;
+    Console.WriteLine(m);
+    result += m;
+}
+Console.WriteLine($"Сумма всех цифр в числе равна {result}");
